4be44a78a2a22e6698ebf0207e3d87ec3785f895
aadionweb07 <108791017+aadionweb07@users.noreply.github.com>
2024-08-16 22:07:53 +0000

    conversion of data

---
+++ 01_basic/03_conversion_operation.js
@@ -0,0 +1,11 @@
+let score = 33
+
+let run = "43"
+console.log(typeof score);
+
+console.log(typeof(run));
+
+
+let valueINnumber= Number(run)
+console.log(typeof valueINnumber);  // 43abc = NaN CHECK   null = 0    undefined = NaN
+
