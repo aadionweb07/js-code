0aa79c78156918e48fc271f7c5b4471423126dd5
aadionweb07 <108791017+aadionweb07@users.noreply.github.com>
2024-08-16 22:25:09 +0000

    comparison

--- 01_basic/03_conversion_operation.js
+++ 01_basic/03_conversion_operation.js
@@ -9,3 +9,29 @@ console.log(typeof(run));
 let valueINnumber= Number(run)
 console.log(typeof valueINnumber);  // 43abc = NaN CHECK   null = 0    undefined = NaN
 
+// /////////////////////////////////////////////////////////////////////////////////////////////
+
+
+let value =3
+let negval = -value
+
+let  str1= "hi"
+let str2 = " hello"
+console.log(str1+str2);
+
+
+
+console.log("1"+2); // 12
+console.log(1+"2"); // 12
+console.log("1"+2+2); //122
+console.log(1+2+"2"); // 32
+
+console.log(+true); // 1
+console.log(+"");
+
+
+
+
+
+
+
---
+++ 01_basic/04_comparison_of_datatype.js
@@ -0,0 +1,18 @@
+console.log(2>1);
+console.log(2<1);
+
+console.log("2">1); // true but may be wrong
+
+
+console.log(null>0); //f
+console.log(null==0);//f
+console.log(null>=0);//t
+
+
+/// === strict check 
+console.log("2"==2);  t
+console.log("2"===2); f
+
+
+
+
