4653b80963aaaa988ec26e6ff886a80d1d1d0ea4
aadionweb07 <108791017+aadionweb07@users.noreply.github.com>
2024-08-16 21:58:13 +0000

    datatype_lesson

--- 01_basic/02_datatype.js
+++ 01_basic/02_datatype.js
@@ -1,2 +1,17 @@
 "use strict" // treat  all js as new 
 // alert(3+3) in node we cant
+let name ="adarsh"
+let age = 18
+let islog = false
+// null => standlone value
+// undefined =>  like if we cant read data
+//symbol => unique used in react
+
+
+//object
+
+console.log(typeof ""); // strings
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
